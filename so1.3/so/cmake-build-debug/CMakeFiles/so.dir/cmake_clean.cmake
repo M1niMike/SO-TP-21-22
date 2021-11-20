cb15ddccee83e2d57b34830864769c14fea5b779
M1niMike <63602790+M1niMike@users.noreply.github.com>
2021-11-20 22:37:35 +0000

    Add files via upload

---
+++ so1.3/so/cmake-build-debug/CMakeFiles/so.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/so.dir/balcao.c.o"
+  "CMakeFiles/so.dir/classifica.c.o"
+  "CMakeFiles/so.dir/medico.c.o"
+  "CMakeFiles/so.dir/utente.c.o"
+  "so"
+  "so.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/so.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
